4e6747b0e3baf1d68b778095c83878c02e17fb9f
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-12-12 13:24:20 +0000

    init

---
+++ js/logic.js
@@ -0,0 +1,40 @@
+var root = document.querySelector(":root");
+
+//functions
+export function append(element, placeToAppend) {
+	return placeToAppend.appendChild (element)
+}
+
+export function updateCssVarAndTextOnChangedInput(label, input, cssVar, addToCssVar) {
+	label.innerHTML = input.value;
+	root.style.setProperty(cssVar, input.value + addToCssVar);//if addToCssVar isnt defined this wile return a error
+}
+
+export function liveInputToCssVarAndLabel (label, input, cssVar, addToCssVar) {
+	updateCssVarAndTextOnChangedInput(label, input, cssVar, addToCssVar);
+	input.oninput = () => {
+		updateCssVarAndTextOnChangedInput(label, input, cssVar, addToCssVar);
+	};
+}
+
+export function CreateSetting(name, cssVarToChange, extraCode, addToVariable, settingType = "range") {
+	var settingsContent = document.querySelector("#settingsContent");
+
+	var settingsContainer = settingsContent.appendChild(document.createElement("label"));
+	settingsContainer.append(name)
+	
+	var settingsInput = settingsContainer.appendChild(document.createElement("input"));
+	settingsInput.setAttribute("type", settingType);
+
+	var settingsLabel = settingsContainer.appendChild(document.createElement("span"))
+
+	extraCode(settingsInput);
+
+	//updating everything when settings changes
+	liveInputToCssVarAndLabel(settingsLabel, settingsInput, cssVarToChange, addToVariable);
+}
+
+// functions for navigation and show or hide system
+function showOrHide (ID) {
+	document.querySelector (ID).classList.toggle("hide");
+}
---
+++ project specefic js.js
@@ -0,0 +1,14 @@
+import * as main from './js/logic.js';
+
+//test setting
+main.CreateSetting(
+	"animation duration",
+	"--transition",
+	function (settingsInput) {
+		settingsInput.setAttribute("min", 0);
+		settingsInput.setAttribute("max", 4);
+		settingsInput.setAttribute("value", 1);
+	},
+	"s",
+	"range"
+);
