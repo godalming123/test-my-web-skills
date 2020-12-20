61c412ad02edd24a933be86a10d2897912202fe4
godalming123 <68993177+godalming123@users.noreply.github.com>
2020-12-20 12:01:21 +0000

    implement parrelex(not working)

--- js/parrelex.js
+++ js/parrelex.js
@@ -1,13 +1,8 @@
-
+parrelex = document.querySelectorAll(".parrelex");
 
 window.addEventListener("scroll", function() {
-  const distance = window.scrollY
-  document.querySelector("header").style.transform = `translateY(${distance *
-    1}px)`
-  document.querySelector(
-    ".container"
-  ).style.transform = `translateY(${distance * 0.3}px)`
-  setTimeout(() => {
-    document.querySelector("section h3").classList.add("animate-me")
-  }, 400)
-})
+  distance = window.scrollY;
+
+	parrelex.style.opacity = `${100-distance}%`;
+	parrelex.style.transform = `translateY(${distance * 0.3}px)`;
+});
